a2074822e31d6b2a062178747385899edea02a15
Samatha <samatha9104@gmail.com>
2024-07-23 00:06:53 +0530

    TestCommit

--- src/Card.js
+++ src/Card.js
@@ -4,91 +4,107 @@ function Card(){
         <div className="card-container">
             <div className="cards">
                 <img className="card-image" src="/profile_logo.png" alt="profile" />
-                <h1 className = "card-title">Scott Ayres</h1>
+                <div className="name">
+                    <h1 className = "card-title">Scott Ayres</h1>
+                    <p>.2nd</p>
+                </div>
                 <p className = "card-text">Busting social media myths at the Social Media Lab,...</p>
                 <div className ="logo-text-container">
                     <img src="profile_logo.png" alt="Logo" className="logo" />
                     <span class="logo-text">23 shared connections</span>
                 </div>
-                <hr />
-                <p className="connect_text">Connect</p>
+                <a href="#">Connect</a>
             </div>
             <div className="cards">
                 <img className="card-image" src="/profile_logo.png" alt="profile" />
-                <h1 className = "card-title">Richard Beeson</h1>
+                <div className="name">
+                    <h1 className = "card-title">Richard Beeson</h1>
+                    <p>.2nd</p>
+                </div>
                 <p className = "card-text">SaaS Customer Onboarding Manager/Producer and...</p>
                 <div className ="logo-text-container">
                     <img src="profile_logo.png" alt="Logo" className="logo" />
                     <span class="logo-text">7 shared connections</span>
                 </div>
-                <hr />
-                <p className="connect_text">Connect</p>
+                <a href="#">Connect</a>
             </div>
             <div className="cards">
                 <img className="card-image" src="/profile_logo.png" alt="profile" />
-                <h1 className = "card-title">Jacob Hilpertsh...</h1>
+                <div className="name">
+                    <h1 className = "card-title">Jacob Hilpertsh...</h1>
+                    <p>.2nd</p>
+                </div>
                 <p className = "card-text">Harness your social media content and engagement..</p>
                 <div className ="logo-text-container">
                     <img src="profile_logo.png" alt="Logo" className="logo" />
                     <span class="logo-text">5 shared connections</span>
                 </div>
-                <hr />
-                <p className="connect_text">Connect</p>
+                <a href="#">Connect</a>
             </div>
             <div className="cards">
                 <img className="card-image" src="/profile_logo.png" alt="profile" />
-                <h1 className = "card-title">Stephanie Swee...</h1>
+                <div className="name">
+                    <h1 className = "card-title">Stephanie Swee...</h1>
+                    <p>.2nd</p>
+                </div>
                 <p className = "card-text">Customer Support/Software QA Tester</p>
                 <div className ="logo-text-container">
                     <img src="profile_logo.png" alt="Logo" className="logo" />
                     <span class="logo-text">4 shared connections</span>
                 </div>
-                <hr />
-                <p className="connect_text">Connect</p>
+                <a href="#">Connect</a>
             </div>
             <div className="cards">
                 <img className="card-image" src="/profile_logo.png" alt="profile" />
-                <h1 className = "card-title">Sarah Hecker</h1>
+                <div className="name">
+                    <h1 className = "card-title">Sarah Hecker</h1>
+                    <p>.2nd</p>
+                </div>
                 <p className = "card-text">US Support & Affiliate Manager at Agorapulse</p>
                 <div className ="logo-text-container">
                     <img src="profile_logo.png" alt="Logo" className="logo" />
                     <span class="logo-text">3 shared connections</span>
                 </div>
-                <hr />
-                <p className="connect_text">Connect</p>
+                <a href="#">Connect</a>
             </div>
             <div className="cards">
                 <img className="card-image" src="/profile_logo.png" alt="profile" />
-                <h1 className = "card-title">Curt Ziegler</h1>
+                <div className="name">
+                    <h1 className = "card-title">Curt Ziegler</h1>
+                    <p>.2nd</p>
+                </div>
                 <p className = "card-text">Customer Support Hero at Agorapulse</p>
                 <div className ="logo-text-container">
                     <img src="profile_logo.png" alt="Logo" className="logo" />
                     <span class="logo-text">3 shared connections</span>
                 </div>
-                <hr />
-                <p className="connect_text">Connect</p>
+                <a href="#">Connect</a>
             </div>
             <div className="cards">
                 <img className="card-image" src="/profile_logo.png" alt="profile" />
-                <h1 className = "card-title">Hannah Recker</h1>
+                <div className="name">
+                    <h1 className = "card-title">Hannah Recker</h1>
+                    <p>.2nd</p>
+                </div>
                 <p className = "card-text">Inbound & Marketing Operations Manager at...</p>
                 <div className ="logo-text-container">
                     <img src="profile_logo.png" alt="Logo" className="logo" />
                     <span class="logo-text">4 shared connections</span>
                 </div>
-                <hr />
-                <p className="connect_text">Connect</p>
+                <a href="#">Connect</a>
             </div>
             <div className="cards">
                 <img className="card-image" src="/profile_logo.png" alt="profile" />
-                <h1 className = "card-title">Michael Angiletta</h1>
+                <div className="name">
+                    <h1 className = "card-title">Michael Angiletta</h1>
+                    <p>.2nd</p>
+                </div>
                 <p className = "card-text">Growth Marketing | CMO | Agorapulse</p>
                 <div className ="logo-text-container">
                     <img src="profile_logo.png" alt="Logo" className="logo" />
                     <span class="logo-text">3 shared connections</span>
                 </div>
-                <hr />
-                <p className="connect_text">Connect</p>
+                <a href="#">Connect</a>
             </div>
         </div>
     )
